2d50c486808f6eb69f4c694e662257a70dc52a3b
Emilie Paillous <emilie.paillous@applidium.com>
2016-04-28 17:27:44 +0200

    send movie path image in json

--- routes/movie.js
+++ routes/movie.js
@@ -3,10 +3,18 @@ var router = express.Router();
 
 var mdb = require('moviedb')('b0b2f356ce291037eef0936cd24034c3');
 
+function image_path(poster_path) {
+  return "http://image.tmdb.org/t/p/w500" + poster_path;
+}
+
 /* GET home page. */
 router.get('/popular', function(req, res, next) {
     var page = (req.query["page"]) ? req.query["page"] : 1;
     mdb.miscPopularMovies({page: page}, function(err, data){
+      data.results.map(function(obj) {
+        obj.poster_path = image_path(obj.poster_path);
+        obj.backdrop_path = image_path(obj.backdrop_path);
+      });
         res.send(data);
     });
 });
@@ -14,6 +22,10 @@ router.get('/popular', function(req, res, next) {
 router.get('/search', function(req, res, next) {
     var page = (req.query["page"]) ? req.query["page"] : 1;
     mdb.searchMovie({query: req.query['q'], page: req.query['page']}, function(err, data){
+      data.results.map(function(obj) {
+        obj.poster_path = image_path(obj.poster_path);
+        obj.backdrop_path = image_path(obj.backdrop_path);
+      });
       res.send(data);
     });
 });
@@ -24,6 +36,10 @@ router.get('/:id(\\d+)/', function(req, res, next) {
     if (err) {
       next(err);
     } else {
+      data.backdrop_path = image_path(data.backdrop_path);
+      data.poster_path = image_path(data.poster_path);
+      data.belongs_to_collection.poster_path = image_path(data.belongs_to_collection.poster_path);
+      data.belongs_to_collection.backdrop_path = image_path(data.belongs_to_collection.backdrop_path);
       res.send(data);
     }
   });
